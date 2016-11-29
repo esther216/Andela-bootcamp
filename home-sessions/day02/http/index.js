254ab69d1b6a11ea0528e0fe3c118449114d2d18
Esther Falayi <aquaster193@gmail.com>
2016-11-29 18:01:52 +0100

    gets followers of a github user

--- home-sessions/day02/http/index.js
+++ home-sessions/day02/http/index.js
@@ -1,22 +1,54 @@
-var gitHub= require('github');
-var prompt= require('prompt');
-var userDetails=[];
+//var gitHub= require('github');
+var inquirer= require('inquirer');
+var prompt = inquirer.createPromptModule();
 
-var properties = [
-  {
-    name: 'username',
-    message: 'Enter Your Github Username or Email Address' 
-  },
-  {
-    name: 'password',
-    hidden: true
-  }
-];
-
-prompt.start();
-
-prompt.get(properties,function(err,result){
-	userDetails.push(result.name);
-	userDetails.push(result.password);
-	console.log("great!");
+var gitHubApi= require('node-github');
+var github = new gitHubApi({
+    version: "3.0.0"
 });
+
+function getUserCredentials(callback) {
+  var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your username or e-mail address';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your password';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
+
+getUserCredentials(function(credentials){
+  //console.log(credentials);
+  github.user.getFollowingFromUser({
+	    user: "mikedeboer"
+	}, function(err, res) {
+			var data= JSON.stringify(res);
+	    console.log(data);
+	});
+});
+
+
+
+
+
