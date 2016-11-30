91056ee4e10fca0a9196a6da6c9e2a15c9e2e486
Esther Falayi <aquaster193@gmail.com>
2016-11-30 16:57:40 +0100

    added test for binary search function

--- home-sessions/day03/tests/testBinarySearch.js
+++ home-sessions/day03/tests/testBinarySearch.js
@@ -1,7 +1,8 @@
-var chai= require('chai');
-var expect= chai.expect;
+'use strict';
+var binarySearch = require ('../app/library.js').binarySearch;
 
-
+var expect = require ('expect');
+binarySearch();
 describe('Binary Search to traverse an ordered list, effectively', function() {
   describe('Populate the arrays with valid content', function() {
     
@@ -13,7 +14,6 @@ describe('Binary Search to traverse an ordered list, effectively', function() {
         expect(oneToTwenty[i + 1] - oneToTwenty[i]).toBe(1);
       }
     });
-
     it('should create an array from 2 to 40, with intervals of 2', function() {
       expect(twoToForty[0]).toBe(2);
       expect(twoToForty[19]).toBe(40);
@@ -22,7 +22,6 @@ describe('Binary Search to traverse an ordered list, effectively', function() {
         expect(twoToForty[i + 1] - twoToForty[i]).toBe(2);
       }
     });
-
     it('should create an array from 10 to 10000, with intervals of 10', function() {
       expect(tenToOneThousand[0]).toBe(10);
       expect(tenToOneThousand[99]).toBe(1000);
@@ -32,78 +31,63 @@ describe('Binary Search to traverse an ordered list, effectively', function() {
       }
     });
   });
-
   describe('Get the index of the item with an expected number of loops in array [1, 2 . . . 20]', function() {
-
     it('should return {count: /* <= 4 */, index: 15} for 16', function() {
       var search  = oneToTwenty.search(16);
       expect(search.count).toBeLessThan(5);
       expect(search.index).toBe(15);
     });
-
     it('should return {count: < /* array length */, index: -1} for 33 ', function () {
       var search = oneToTwenty.search(33);
       expect(search.count).toBeLessThan(search.length);
       expect(search.index).toBe(-1);
     });
   });
-
   
   it('should not contain any form of `indexOf` in the search function', function() {
     expect(Array.prototype.search.toString().indexOf('indexOf')).toBe(-1);      
   });
   
   describe('Get the index of the item with an expected number of loops in array [2, 4 . . . 40]', function() {
-
     it('should return {count: /* <= 4 */, index: 15} for 16', function() {
       var search  = twoToForty.search(16);
       expect(search.count).toBeLessThan(5);
       expect(search.index).toBe(7);
     });
-
     it('should return {count: 0, index: 9} for 20', function() {
       var search  = twoToForty.search(20);
       expect(search.count).toBe(0);
       expect(search.index).toBe(9);
     });
-
-
     it('should return {count: 0, index: 19} for 40', function() {
       var search  = twoToForty.search(40);
       expect(search.count).toBe(0);
       expect(search.index).toBe(19);
     });
-
     it('should return {count: < /* array length */, index: -1} for 33 ', function () {
       var search = twoToForty.search(33);
       expect(search.count).toBeLessThan(search.length);
       expect(search.index).toBe(-1);
     });
   });
-
   describe('Get the index of the item with an expected number of loops in array [10, 20 . . . 1000]', function() {
-
     it('should return {count: /* <= 3 */, index: 3} for 40', function() {
       var search  = tenToOneThousand.search(40);
       expect(search.count).toBeLessThan(4);
       expect(search.index).toBe(3);
     });
-
     it('should return {count: /* <= 5*/, index: 87} for 800', function() {
       var search  = tenToOneThousand.search(880);
       expect(search.count).toBeLessThan(6);
       expect(search.index).toBe(87);
     });
-
     it('should return {count: < /* array length */, index: -1} for 10000 ', function () {
       var search = tenToOneThousand.search(10000);
       expect(search.count).toBeLessThan(search.length);
       expect(search.index).toBe(-1);
     });
-
   });
 });
-
 /*
 * Block the indexOf Array function
 */
@@ -112,4 +96,4 @@ Array.prototype.indexOf = function () {
 };
 var oneToTwenty = [].toTwenty();
 var twoToForty = [].toForty();
-var tenToOneThousand = [].toOneThousand();
+var tenToOneThousand = [].toOneThousand()
