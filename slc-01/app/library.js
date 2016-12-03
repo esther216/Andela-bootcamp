81605e35bdd63bd94c2af844750bf1be5f3113ff
Esther Falayi <aquaster193@gmail.com>
2016-12-03 20:34:28 +0100

    added aritGeo function and tested

--- slc-01/app/library.js
+++ slc-01/app/library.js
@@ -1,3 +1,38 @@
 module.exports={
-	
+	aritGeo: function(data){
+		var len= data.length;
+		if(len === 0){
+			return 0;
+		}
+		else{
+			//calculate common difference
+      var diff= data[1] - data[0];
+         
+      // calculate common ratio
+      var ratio= data[1]/data[0];
+         
+      //calculate sum of AP series
+      var sumAP= len*(2*data[0] + (len -1)*diff)/2;
+         
+      //calculate sum of GP series
+      var sumGP= (data[0]*(1- Math.pow(ratio, len)))/(1 - ratio);
+         
+      //initialise computed sum
+      var realSum= 0;
+      for(var i=0; i < len; i++){
+        realSum += data[i];
+      }
+         
+      //compare values
+      if(realSum == sumAP){
+        return "Arithmetic";
+      }
+      else if( realSum == sumGP){
+        return "Geometric";
+      }
+      else{
+        return -1;
+      }
+		}
+	}
 }
